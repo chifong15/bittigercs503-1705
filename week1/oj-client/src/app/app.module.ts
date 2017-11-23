d5f0ef9ff0a662ab49f64a0a4683da5a79bf416a
chifong15 <chihangfongsjsu@gmail.com>
2017-11-22 18:10:04 -0800

    added nav

--- week1/oj-client/src/app/app.module.ts
+++ week1/oj-client/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+import { routing } from './app.routes';
 import { DataService } from './services/data.service';
 import { AppComponent } from './app.component';
 import { ProblemListComponent } from './components/problem-list/problem-list.component';
@@ -18,7 +20,9 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing,
+    FormsModule,
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
--- week1/oj-client/src/app/components/new-problem/new-problem.component.ts
+++ week1/oj-client/src/app/components/new-problem/new-problem.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Problem } from '../../models/problem.model';
+import { DataService } from '../../services/data.service';
+
+const DEFAULT_PROBLEM: Problem = Object.freeze({
+  id: 0,
+  name: '',
+  desc: '',
+  difficulty: 'easy'
+});
 
 @Component({
   selector: 'app-new-problem',
@@ -6,10 +15,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-problem.component.css']
 })
 export class NewProblemComponent implements OnInit {
-
-  constructor() { }
+  newProblem: Problem = Object.assign({}, DEFAULT_PROBLEM);
+  difficulties: string[] = ['easy','medium','hard','super'];
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
   }
 
+
+  addProblem(){
+    this.dataService.addProblem(this.newProblem);
+    this.newProblem = Object.assign({}, DEFAULT_PROBLEM);
+  }
 }
